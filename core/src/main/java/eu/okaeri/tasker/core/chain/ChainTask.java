6437c199c4a0e44508e6a35e12e807da377a4f07
PinkLolicorn <pinklolicorngithub@daffit.pl>
2023-02-25 11:22:15 +0100

    Allow runtime task inserts, delay, overhaul method names - rename conflicting acceptX(Function) methods to transformX - remove callback parameter from TaskerExecutor#run - add TaskerChain#delay - add TaskerChain#unsafe - add TaskerChain#unsafeGet - add TaskerExecutor#runLater

--- core/src/main/java/eu/okaeri/tasker/core/chain/ChainTask.java
+++ core/src/main/java/eu/okaeri/tasker/core/chain/ChainTask.java
@@ -3,11 +3,13 @@ package eu.okaeri.tasker.core.chain;
 import lombok.Data;
 import lombok.experimental.Delegate;
 
+import java.time.Duration;
 import java.util.function.Supplier;
 
 @Data
 class ChainTask {
     @Delegate protected final Runnable runnable;
+    protected final Duration delay;
     protected final Supplier<Boolean> async;
     protected final boolean exceptionHandler;
 }
