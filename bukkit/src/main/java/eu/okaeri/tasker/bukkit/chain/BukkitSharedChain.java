8898f76f5771afb74a5b070479b410457bbe8fa9
Sandra <sandra@okaeri.cloud>
2024-08-26 22:21:46 +0200

    Include all SharedChain methods in queued execution

--- bukkit/src/main/java/eu/okaeri/tasker/bukkit/chain/BukkitSharedChain.java
+++ bukkit/src/main/java/eu/okaeri/tasker/bukkit/chain/BukkitSharedChain.java
@@ -2,36 +2,69 @@ package eu.okaeri.tasker.bukkit.chain;
 
 import eu.okaeri.tasker.bukkit.BukkitTasker;
 import lombok.NonNull;
+import lombok.SneakyThrows;
+import lombok.experimental.Delegate;
 
 import java.util.Queue;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 public class BukkitSharedChain<T> extends BukkitTaskerChain<T> {
 
+    protected @Delegate(excludes = Accessors.class) BukkitTaskerChain<T> delegate;
     protected final Queue<Runnable> queue;
-    protected final AtomicBoolean executed = new AtomicBoolean(false);
+    protected boolean executed = false;
 
-    public BukkitSharedChain(@NonNull BukkitTasker tasker, @NonNull Queue<Runnable> queue) {
-        super(tasker);
+    private interface Accessors<T> {
+        void execute(@NonNull Consumer<T> consumer);
+        void execute();
+        Future<T> executeFuture();
+        T await();
+        T await(long timeout, TimeUnit unit);
+    }
+
+    public BukkitSharedChain(@NonNull BukkitTaskerChain<T> delegate, @NonNull Queue<Runnable> queue) {
+        super(((BukkitTasker) delegate.getTasker()));
+        this.delegate = delegate;
         this.queue = queue;
     }
 
-    @Override
-    public void execute() {
-        if (this.executed.get()) {
+    protected void queueWith(@NonNull Runnable runnable) {
+        if (this.executed) {
             throw new RuntimeException("Cannot execute already executed chain");
         }
-        this.executed.set(true);
-        this.queue.add(super::await);
+        this.executed = true;
+        this.queue.add(runnable);
     }
 
     @Override
     public void execute(@NonNull Consumer<T> consumer) {
-        if (this.executed.get()) {
-            throw new RuntimeException("Cannot execute already executed chain");
-        }
-        this.executed.set(true);
-        this.queue.add(() -> consumer.accept(super.await()));
+        this.queueWith(() -> consumer.accept(this.delegate.await()));
+    }
+
+    @Override
+    public void execute() {
+        this.queueWith(this.delegate::await);
+    }
+
+    @Override
+    public Future<T> executeFuture() {
+        CompletableFuture<T> future = new CompletableFuture<>();
+        this.queueWith(() -> future.complete(this.delegate.await()));
+        return future;
+    }
+
+    @Override
+    @SneakyThrows
+    public T await() {
+        return this.executeFuture().get();
+    }
+
+    @Override
+    @SneakyThrows
+    public T await(long timeout, TimeUnit unit) {
+        return this.executeFuture().get(timeout, unit);
     }
 }
